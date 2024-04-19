26d02aa3b678d6a57119dc1aa8b87a9465568977
jurgenvrapi97 <jurgenvrapi@gmail.com>
2024-04-19 10:05:10 +0200

    Initial commit

---
+++ src/main/java/jurgenvrapi/S7L5progetto/S7L5ProgettoApplication.java
@@ -0,0 +1,13 @@
+package jurgenvrapi.S7L5progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7L5ProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7L5ProgettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jurgenvrapi/S7L5progetto/S7L5ProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package jurgenvrapi.S7L5progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7L5ProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
